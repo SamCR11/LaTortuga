6cc0af549867749f9a9fb5d400dddb10ff9f2d2a
Sam 23 <124098608+SamCR11@users.noreply.github.com>
2025-04-01 10:23:59 -0500

    agregamos cambios

---
+++ main.js
@@ -0,0 +1,9 @@
+function registrar(){
+    document.getElementById('part').style.display = 'none'
+    document.getElementById('proo').style.display = 'block' 
+}
+
+function validar(){
+    document.getElementById('part').style.display = 'none'
+    document.getElementById('nuub').style.display = 'block' 
+}
